2ed5daa15bdfaeaa6b6ae1c88b0b77fd4ad58def
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-04 15:38:51 +0530

    v9: Resolving errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
         Mailx.signup(@user).deliver

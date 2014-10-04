605640522e1d183b9fe7b232a302e00037835048
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-04 23:50:26 +0530

    v9: Resolving errors

---
+++ vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:authentication => :plain,
+	:domain => 'heroku.com',
+	:user_name => shrey14099@iiitd.ac.in,
+	:password =>shrey1234@,
+}

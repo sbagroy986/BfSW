de6fa3cef201ef7fc13395e0f1a480666252eca3
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-04 20:15:27 +0530

    v9: Resolving errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ BfSW::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address: 'smtp.sendgrid.net',
+port: 587,
+domain: 'heroku.com',
+authentication: "plain",
+user_name: "shrey14099@iiitd.ac.in",
+password: "shrey1234@",
+enable_starttls_auto: true
+}
 end

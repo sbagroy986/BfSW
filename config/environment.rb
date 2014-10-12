e30f88896f21732e4fbe4c3ff7480a3551b61188
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-12 18:50:57 +0530

    v10:Made some changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
+end

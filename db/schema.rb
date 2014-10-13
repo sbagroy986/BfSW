082a556339dec6e3569d12d66cdd6127af8cc4c1
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-13 12:41:17 +0530

    v10: Resolving some error.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010062442) do
+ActiveRecord::Schema.define(version: 20141013065928) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.text     "about"
+    t.string   "password_digest"
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"

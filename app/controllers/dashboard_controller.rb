bb7babcda9fb5bf0fbb5db999b4bbc13b9c81143
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-14 10:54:42 +0530

    v10: Resolving some issues

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
 skip_before_action :authorize
   def view
+  @user = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   mount_uploader :image, AvatarUploader
+  def self.search(query)
+  	where("email like ?","%#{query}%")
+  end
 end

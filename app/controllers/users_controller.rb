a3cb66953f487710a6d7fd95c90638e54c9257be
sbagroy986 <shrey14099@iiitd.ac.in>
2014-10-17 08:56:48 +0530

    v10: Resolving some issues.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,12 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    def index
   if params[:search]
-    @users = User.search(params[:search]).order("created_at DESC")
+    @users = User.search(params[:search]).order("created_at DESC").all
   else
     @users = User.all.order('created_at DESC')
   end
 end
-  end
 
   # GET /users/1
   # GET /users/1.json
